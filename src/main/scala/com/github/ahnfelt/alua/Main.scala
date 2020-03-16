11a9c668126550c7bf6ec2b260647ae9817783cd
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2020-03-16 20:47:43 +0100

    Tokenizer - poor mans benchmark. 5 mio lines / s?

--- src/main/scala/com/github/ahnfelt/alua/Main.scala
+++ src/main/scala/com/github/ahnfelt/alua/Main.scala
@@ -8,15 +8,20 @@ import com.github.ahnfelt.alua.language.Tokenizer.Token
 object Main {
     def main(arguments : Array[String]) : Unit = {
         val bytes = Files.readAllBytes(Paths.get(arguments(0)))
-        process(bytes)
+        val moreBytes = (for(i <- 1 to 10000) yield bytes).toArray.flatten
+        println(moreBytes.length + " bytes, " + moreBytes.count(_ == '\n') + " lines")
+        for(i <- 1 to 10) process(moreBytes)
     }
 
     def process(bytes : Array[Byte]) : Unit = {
         val tokens = measure("tokenize", Tokenizer.tokenize(bytes))
+        var count = 0L
         for(tokenBits <- tokens.drop(8)) {
             val token = new Token(tokenBits)
+            count += tokenBits
             //println(token.kind + " " + token.offset + " " + token.length)
         }
+        println(count)
     }
 
     def measure[T](label : String, body : => T) = {
