3fa1a5217bc2185270089473d8bbf89f0dbfab93
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2020-03-16 20:10:25 +0100

    Tokenizer

--- src/main/scala/com/github/ahnfelt/alua/Main.scala
+++ src/main/scala/com/github/ahnfelt/alua/Main.scala
@@ -5,8 +5,6 @@ import java.nio.file.{Files, Path, Paths}
 import com.github.ahnfelt.alua.language.Tokenizer
 import com.github.ahnfelt.alua.language.Tokenizer.Token
 
-import scala.io.Source
-
 object Main {
     def main(arguments : Array[String]) : Unit = {
         val bytes = Files.readAllBytes(Paths.get(arguments(0)))
